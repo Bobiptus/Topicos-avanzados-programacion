6e770074c4f65945a70c54b10982f2c96a15c551
Bobiptus <pb.armando@gmail.com>
2025-02-22 16:05:28 -0800

    Se agrego cambio de color y doble click

--- Practica #2.py
+++ Practica #2.py
@@ -1,14 +1,24 @@
 from tkinter import *
 import tkinter
+import random
 
 # Crear una ventana
 ventana = tkinter.Tk() 
 ventana.title("Eventos del mouse y teclado")
 ventana.geometry("500x500")
 
+# Función para cambiar el fondo de la ventana
+def cambiar_fondo():
+    colores = ["red", "blue", "green", "yellow", "orange", "purple"]
+    color = random.choice(colores)
+    ventana.config(bg = color)
+
 # Definiendo los eventos del mouse
 def click_izq(event):
     etiqueta_eventos.config(text=f"Click izquierdo en ({event.x}, {event.y})")
+    
+def double_click(event):
+    etiqueta_eventos.config(text=f"Doble click en ({event.x}, {event.y})")
 
 def click_der(event):
     etiqueta_eventos.config(text=f"Click derecho en ({event.x}, {event.y})")
@@ -19,6 +29,7 @@ def arrastrar(event):
 # Asociando el mouse a las funciones
 ventana.bind("<Button-1>", click_izq)
 ventana.bind("<Button-3>", click_der)
+ventana.bind("<Double-Button-1>", double_click)
 ventana.bind("<B1-Motion>", arrastrar)
 
 # Definiendo eventos del teclado
@@ -44,6 +55,10 @@ etiqueta_titulo.pack(pady=10)
 # Etiqueta específica para mostrar los eventos sin sobrescribir el título
 etiqueta_eventos = tkinter.Label(ventana, text="Aquí se mostrarán los eventos", font=("Arial", 12), fg="green")
 etiqueta_eventos.pack(pady=20)
+
+# Crear un boton para cambiar el color
+boton2 = tkinter.Button(ventana, text = "Cambiar color", command=cambiar_fondo) # Crear un boton para cambiar el color
+boton2.pack(pady=10)
 
 # Crear un boton para salir
 boton_salir = tkinter.Button(ventana, text = "Salir", command=ventana.quit) 
